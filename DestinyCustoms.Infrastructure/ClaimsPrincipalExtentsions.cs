22abb1fcdd27e825ac5c311491aef38489adc251
Tsvetelin-Crimson <67798636+Tsvetelin-Crimson@users.noreply.github.com>
2021-07-27 17:43:50 +0300

    Moved services to a different project (class library)
    Added references where needed.

---
+++ DestinyCustoms.Infrastructure/ClaimsPrincipalExtentsions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace DestinyCustoms.Infrastructure
+{
+    public static class ClaimsPrincipalExtentsions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    }
+}
